f2de98a15726d42ba0a53f0fc6e36827b7e4e7fa
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2020-04-03 09:48:59 +0530

    Add files via upload

---
+++ Invert_a_Tree.java
@@ -0,0 +1,10 @@
+ public TreeNode invertTree(TreeNode root) {
+        if(root==null)
+            return null;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        root.left=invertTree(root.left);
+        root.right=invertTree(root.right);
+        return root;
+    }
