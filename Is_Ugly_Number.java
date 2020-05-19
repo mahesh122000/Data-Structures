4f8ccd451f34f97a6aa8188dd6e8b561547afdaf
P.Mahesh <46820201+mahesh122000@users.noreply.github.com>
2020-05-19 10:06:35 +0530

    Add files via upload

---
+++ Is_Ugly_Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isUgly(int n) {
+        while(n>0 && n%2==0)
+        {n=n/2;}
+        while(n>0 && n%3==0)
+        {n=n/3;}
+        while(n>0 && n%5==0)
+        {n=n/5;}
+        return n==1;
+    }
+}
